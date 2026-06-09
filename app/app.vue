063af2dae8801b24f469968d6332c99902027efd
hebu <2rwin_rommel@naver.com>
2026-06-09 17:32:15 +0900

    feat: PageWrapper for root layout - add panel layout - add marquee

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,18 @@
+<script setup lang="ts">
+import PageWrapper from './components/PageWrapper.vue';
+</script>
+
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+      <main class="main-content">
+            <PageWrapper>
+                  <NuxtPage />
+            </PageWrapper>
+      </main>
 </template>
+
+<style scoped>
+.main-content {
+      width: 100%;
+      min-height: 100dvh;
+}
+</style>
