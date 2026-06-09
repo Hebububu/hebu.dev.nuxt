56d57a6e0bfca0b21115acccac91f5f11c16947f
hebu <2rwin_rommel@naver.com>
2026-06-09 14:19:28 +0900

    chore: add @nuxt/fonts module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,6 +7,8 @@ export default defineNuxtConfig({
       ]
     }
   },
+
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: ['@nuxt/fonts']
 })
