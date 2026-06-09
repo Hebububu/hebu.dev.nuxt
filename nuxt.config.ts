2e12319d746a00e3af57de553732412def9a0350
hebu <2rwin_rommel@naver.com>
2026-06-09 13:38:49 +0900

    chore: add pre-bundle vue dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  vite: {
+    optimizeDeps: {
+      include: [
+        '@vue/devtools-core',
+        '@vue/devtools-kit',
+      ]
+    }
+  },
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true }
 })
