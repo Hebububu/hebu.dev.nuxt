a7aac800a5c20ff0d541c286532e95a7887afac8
hebu <2rwin_rommel@naver.com>
2026-06-09 14:27:52 +0900

    chore: add root font

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,5 +10,6 @@ export default defineNuxtConfig({
 
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/fonts']
+  modules: ['@nuxt/fonts'],
+  css: ['~/assets/css/main.css']
 })
